d345d6e852c1e2ac4889370427a519e33655c584
istaqom <istaqom@gmail.com>
2023-11-03 02:44:30 +0800

    praktikum use state dan consume api

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import MainScreen from './screen/mainScreen'
 import TestScreen from './screen/testScreen'
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/screen/testScreen.jsx
+++ src/screen/testScreen.jsx
@@ -1,12 +1,39 @@
+import { useEffect, useState } from 'react';
 import FooterComponent from '../components/footer.jsx'
 import NavBar from '../components/navbar.jsx';
+import fetchApi from '../utils/fetchApi.js';
 
 function TestScreen() {
+    const [count,setCount] = useState(0)
+    const [data,setData] = useState()
+
+    useEffect(() => {
+        fetchApi({temp : 'people', id : 1}).then((response) => response.json()).then((data) => setData(data.results)).catch(error => console.log(error))
+    },[])
     return (
         <>
             <NavBar/>
             <div className='bg-orange-50 flex flex-col text-black'>
-                <h1>Test Screen 2</h1>
+                <h1>Test Screen 2</h1>      
+            </div>
+            <div className='flex justify-center'>
+                <p className='text-white text-5xl'>Count Number : <span>{count}</span></p>
+                <button onClick={(e) => {
+                    e.preventDefault(),
+                    setCount(count+1)
+                }}>Increment</button>
+                <button onClick={(e) => {
+                    e.preventDefault(),
+                    setCount(count-1)
+                }}>Decrement</button>
+            </div>
+            <div>
+                {data ? <>                
+                {data.map((item,index) => {
+                    return <p key={index}>{item.name}</p>
+                })}
+                </> : <></>}
+                <p></p>
             </div>
             <FooterComponent/>
         </>
